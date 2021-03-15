e597854e3231b9ac05041aa499e286004c60d04b
nitaking <10850034+nitaking@users.noreply.github.com>
2021-03-16 00:03:21 +0900

    Install audio-recorder-polyfill

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,57 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState} from 'react';
+
 
 export default function Home() {
+  const [audio, setAudio] = useState()
+  const [isNowRecording, setIsNowRecording] = useState(false)
+
+  let recorder
+
+  function onStart() {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      recorder = new MediaRecorder(stream)
+
+      recorder.addEventListener('dataavailable', e => {
+        // e.data にデータが入っているのでデータ処理はこちらから
+        setAudio(e.data)
+
+      })
+
+      recorder.start()
+      // setIsNowRecording(true)
+    }).catch((e) => {
+      throw new Error('録音に失敗しました')
+    })
+  }
+  function onPause() {
+    recorder?.pause()
+  }
+  function onResume() {
+    recorder?.resume()
+  }
+  function onStop() {
+    recorder?.stop()
+    // setIsNowRecording(false)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
+        <script>
+          {
+            () => {
+              // CSRでscriptを実行するために行う
+              if (!window.MediaRecorder) {
+                document.write(
+                  decodeURI('%3Cscript defer src="/polyfill.js">%3C/script>')
+                )
+              }
+            }
+          }
+        </script>
+
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -14,39 +61,27 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.card}>
+            <button onClick={onStart} type="button" className="m-1">Record</button>
+            <button onClick={onPause} type="button" className="m-1">Pause</button>
+            <button onClick={onResume} type="button" className="m-1">Resume</button>
+            <button onClick={onStop} type="button" className="m-1">Stop</button>
+          </div>
+          <div className={styles.card}>
+            <audio src={audio && URL.createObjectURL(audio)} controls />
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {isNowRecording ? <p>録音中</p> : <p>録音可能</p>}
+          <div className={styles.card}>
+          </div>
         </div>
       </main>
 
